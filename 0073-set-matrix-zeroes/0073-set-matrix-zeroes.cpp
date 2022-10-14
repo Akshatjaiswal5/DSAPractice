66bcab042b11acfa816e513e79d93a303faacfef
Akshat Jaiswal <68910240+Akshatjaiswal5@users.noreply.github.com>
2022-10-15 03:33:22 +0530

    Time: 38 ms (15.28%), Space: 13.3 MB (54.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-      int x,y;
+      int x,y,n=matrix.size(),m=matrix[0].size();
       
       bool flag=false;
-      for(int i=0;i<matrix.size();i++)
+      for(int i=0;i<n;i++)
       {
-       for(int j=0;j<matrix[0].size();j++)
+       for(int j=0;j<m;j++)
         if(matrix[i][j]==0) 
         {
          x=i;y=j;
@@ -19,28 +19,28 @@ public:
       if(!flag)
         return;
       
-      for(int i=0;i<matrix.size();i++)
-        for(int j=0;j<matrix[0].size();j++)
+      for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
           if(matrix[i][j]==0)
           {
             matrix[x][j]=0;
             matrix[i][y]=0;
           }
       
-      for(int i=0;i<matrix.size();i++)
+      for(int i=0;i<n;i++)
         if(matrix[i][y]==0&&i!=x)
-          for(int j=0;j<matrix[0].size();j++)
+          for(int j=0;j<m;j++)
               matrix[i][j]=0;
       
-      for(int j=0;j<matrix[0].size();j++)
+      for(int j=0;j<m;j++)
         if(matrix[x][j]==0)
-          for(int i=0;i<matrix.size();i++)
+          for(int i=0;i<n;i++)
              matrix[i][j]=0;
       
-      for(int i=0;i<matrix.size();i++)
+      for(int i=0;i<n;i++)
         matrix[i][y]=0;
       
-      for(int j=0;j<matrix[0].size();j++)
+      for(int j=0;j<m;j++)
         matrix[x][j]=0;
     }
 };
